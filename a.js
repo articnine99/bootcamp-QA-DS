f49f30d84941860a54ce7dedadf9e515cbd47295
Akbar Saputra <akbarsaputra1003@gmail.com>
2024-12-13 14:55:01 +0700

    Menambahkan file baru untuk di ubah

--- a.js
+++ a.js
@@ -1,1 +1,107 @@
-// bree
+const schema = {
+  type: "object",
+  properties: {
+    page: {
+      type: "number",
+    },
+    per_page: {
+      type: "number",
+    },
+    total: {
+      type: "number",
+    },
+    total_pages: {
+      type: "number",
+    },
+    data: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          id: {
+            type: "number",
+          },
+          email: {
+            type: "string",
+          },
+          first_name: {
+            type: "string",
+          },
+          last_name: {
+            type: "string",
+          },
+          avatar: {
+            type: "string",
+          },
+        },
+      },
+    },
+    support: {
+      type: "object",
+      properties: {
+        url: {
+          type: "string",
+        },
+        text: {
+          type: "string",
+        },
+      },
+    },
+  },
+};
+
+const schemaSingleUser = {
+  type: "object",
+  properties: {
+    data: {
+      type: "object",
+      properties: {
+        id: {
+          type: "number",
+        },
+        email: {
+          type: "string",
+        },
+        first_name: {
+          type: "string",
+        },
+        last_name: {
+          type: "string",
+        },
+        avatar: {
+          type: "string",
+        },
+      },
+    },
+    support: {
+      type: "object",
+      properties: {
+        url: {
+          type: "string",
+        },
+        text: {
+          type: "string",
+        },
+      },
+    },
+  },
+};
+
+const schemaUpdateUser = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+    },
+    job: {
+      type: "string",
+    },
+    updatedAt: {
+      type: "string",
+    },
+  },
+};
+
+pm.test("Validate JSON Schema", function () {
+  pm.response.to.have.jsonSchema(schema);
+});
--- b.js
+++ b.js
@@ -1,1 +1,62 @@
-// awas
+const schema = {
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      id: {
+        type: "string",
+      },
+      name: {
+        type: "string",
+      },
+      data: {
+        type: ["object", "null"],
+        properties: {
+          color: {
+            type: "string",
+          },
+          capacity: {
+            type: ["string", "number"],
+          },
+          price: {
+            type: "number",
+          },
+          generation: {
+            type: "string",
+          },
+          year: {
+            type: "number",
+          },
+          "CPU model": {
+            type: "string",
+          },
+          "Hard disk size": {
+            type: "string",
+          },
+          "Strap Colour": {
+            type: "string",
+          },
+          "Case Size": {
+            type: "string",
+          },
+          Description: {
+            type: "string",
+          },
+          Capacity: {
+            type: "string",
+          },
+          "Screen size": {
+            type: "number",
+          },
+        },
+        additionalProperties: true,
+      },
+    },
+    required: ["id", "name", "data"],
+    additionalProperties: false,
+  },
+};
+
+pm.test("Validate JSON Schema", function () {
+  pm.response.to.have.jsonSchema(schema);
+});
